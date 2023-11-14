1507a2b549e85b15d244aab8bb3769ef2ddb6d66
L Carver <wifihum@gmail.com>
2023-11-14 12:55:36 -0800

    put file utility functions in a separate .cpp file, add .h  file

--- ex12_file_io.cpp
+++ ex12_file_io.cpp
@@ -4,7 +4,6 @@
 // -Read from text file into vector, 1 string element per line
 // -Write vector elements 
 // -Display each vector element, from highest to lowest index
-// -Uses global namespace infile, so functions share ifstream
 // 
 // -#TODO This code deletes all vector elements with pop_back() - fix this.
 // -#TODO Add a function to output each vector element to a file. 
@@ -12,35 +11,15 @@
 //------------------------------------------------------------------------------
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <vector>
+
+#include "ut_file_io.h"
 
 using namespace std;
 
 //------------------------------------------------------------------------------
-// toggle DEBUG mode
-//------------------------------------------------------------------------------
-#define _LCDEBUG
-
-//------------------------------------------------------------------------------
-// file metadata
-//------------------------------------------------------------------------------
-namespace f {
-    const string INPUT_FILENAME = "ex12_data.csv";
-    ifstream input_file;
-
-    const string OUTPUT_FILENAME = "ex12_output.txt";
-    ofstream output_file;
-
-    constexpr int INPUT_FILE_ERROR = 1;
-    constexpr int OUTPUT_FILE_ERROR = 2;
-}
-
-//------------------------------------------------------------------------------
 // local functions
 //------------------------------------------------------------------------------
-void open_input_file();
-void read_input_file(vector<string>&);
 void display_file_data(vector<string>&); // #TODO fix data loss
 
 //------------------------------------------------------------------------------
@@ -48,53 +27,13 @@ void display_file_data(vector<string>&); // #TODO fix data loss
 //------------------------------------------------------------------------------
 int main() {
 
-    open_input_file();
-
     // read file data into vector one line at a time
     vector<string> vs;
     read_input_file(vs);
 
-    // be sure to close the file!
-    f::input_file.close();
-
     display_file_data(vs);
 
     return 0;
-}
-
-//------------------------------------------------------------------------------
-// -opens passed file name for reading
-// -uses global ifstream infile::input_file
-// -exits application on file open error
-//------------------------------------------------------------------------------
-void open_input_file() {
-
-    f::input_file.open(f::INPUT_FILENAME);
-
-    if (!f::input_file.is_open()) {
-
-        cerr << "Could not open input file: " << f::INPUT_FILENAME << ".\n";
-        exit(f::INPUT_FILE_ERROR);
-    }
-}
-
-//------------------------------------------------------------------------------
-// -reads each line of open input file
-// -stores string data in passed vector
-//------------------------------------------------------------------------------
-void read_input_file(vector<string>& v) {
-
-    while (f::input_file.good()) {
-
-        string input_line;
-        getline(f::input_file, input_line);
-
-#ifdef _LCDEBUG
-        cout << "read from file: " << input_line << '\n';
-#endif
-
-        v.push_back(input_line);
-    }
 }
 
 //------------------------------------------------------------------------------
---
+++ ut_file_io.cpp
@@ -0,0 +1,80 @@
+//------------------------------------------------------------------------------
+// ut_file_io.cpp
+//
+// -Utility functions read and write from text file into vector
+// 
+// -#TODO This code deletes all vector elements with pop_back() - fix this.
+// -#TODO Add a function to output each vector element to a file. 
+//      Start your research here: https://cplusplus.com/doc/tutorial/files/
+//------------------------------------------------------------------------------
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+// toggle DEBUG mode
+//------------------------------------------------------------------------------
+#define _LCDEBUG
+
+//------------------------------------------------------------------------------
+// file metadata
+//------------------------------------------------------------------------------
+namespace f {
+    const string INPUT_FILENAME = "ex12_data.csv";
+    ifstream input_file;
+
+    const string OUTPUT_FILENAME = "ex12_output.txt";
+    ofstream output_file;
+
+    constexpr int INPUT_FILE_ERROR = 1;
+    constexpr int OUTPUT_FILE_ERROR = 2;
+}
+
+//------------------------------------------------------------------------------
+// local functions
+//------------------------------------------------------------------------------
+static void open_input_file();
+
+//------------------------------------------------------------------------------
+// -opens passed file name for reading
+// -uses global ifstream infile::input_file
+// -exits application on file open error
+//------------------------------------------------------------------------------
+static void open_input_file() {
+
+    f::input_file.open(f::INPUT_FILENAME);
+
+    if (!f::input_file.is_open()) {
+
+        cerr << "Could not open input file: " << f::INPUT_FILENAME << ".\n";
+        exit(f::INPUT_FILE_ERROR);
+    }
+}
+
+//------------------------------------------------------------------------------
+// -reads each line of open input file
+// -stores string data in passed vector
+//------------------------------------------------------------------------------
+void read_input_file(vector<string>& v) {
+
+    open_input_file();
+
+    while (f::input_file.good()) {
+
+        string input_line;
+        getline(f::input_file, input_line);
+
+#ifdef _LCDEBUG
+        cout << "read from file: " << input_line << '\n';
+#endif
+
+        v.push_back(input_line);
+    }
+
+    // be sure to close the file!
+    f::input_file.close();
+}
+
